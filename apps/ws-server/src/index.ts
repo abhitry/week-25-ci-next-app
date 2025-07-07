3bc64be8ad578c9a4a5b2eeaff5d8cfff7fb6880
Abhishek Masne <abhishekmasne2015@gmail.com>
2025-07-07 11:16:35 +0530

    completed the app logic

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+import {client} from "@repo/db/client2";
+
+const server = new WebSocketServer({ port: 3001 });
+
+server.on("connection", async(socket) => {
+    await client.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    });
+    socket.send("New client connected");
+
+});
